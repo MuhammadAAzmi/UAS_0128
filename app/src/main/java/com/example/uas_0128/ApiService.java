8dde3f0b4ef98b30d33bc168dda68d455563658c
MuhammadAAzmi <Muhammad.1405@students.amikom.ac.id>
2021-01-16 21:58:42 +0700

    UAS_0128

---
+++ app/src/main/java/com/example/uas_0128/ApiService.java
@@ -0,0 +1,14 @@
+package com.example.uas_0128;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiService {
+    @GET("api/v1/json/1/eventspastleague.php")
+    Call<MatchResponse> getLastMatch(@Query("id") String id);
+
+    @GET("api/v1/json/1/eventsnextleague.php")
+    Call<MatchResponse> getNextMatch(@Query("id") String id);
+
+}
---
+++ app/src/main/java/com/example/uas_0128/ui/main/LastMatchFragment.java
@@ -0,0 +1,60 @@
+package com.example.uas_0128.ui.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.uas_0128.ApiClient;
+import com.example.uas_0128.ApiService;
+import com.example.uas_0128.LastMatchAdapter;
+import com.example.uas_0128.Match;
+import com.example.uas_0128.MatchResponse;
+import com.example.uas_0128.R;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class LastMatchFragment extends Fragment {
+
+    private ArrayList<Match> listMatch;
+    private RecyclerView rvMatch;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_last_match, container, false);
+        rvMatch = view.findViewById(R.id.rv_lastmatch);
+        rvMatch.setHasFixedSize(true);
+        rvMatch.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        ApiService service = ApiClient.getRetrofitInstance().create(ApiService.class);
+        Call<MatchResponse> call = service.getLastMatch("4328");
+        call.enqueue(new Callback<MatchResponse>() {
+            @Override
+            public void onResponse(Call<MatchResponse> call, Response<MatchResponse> response) {
+
+                listMatch = response.body().getEvents();
+
+                LastMatchAdapter lastMatchAdapter = new LastMatchAdapter(listMatch);
+                rvMatch.setAdapter(lastMatchAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<MatchResponse> call, Throwable t) {
+
+            }
+        });
+
+
+
+        return view;
+    }
+}
